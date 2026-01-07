d82ee1a322f011115bb4db0543df47ec0a3b1a83
unknown <swathesh.ags@gmail.com>
2026-01-07 12:10:11 +0530

    bootstrap chanes

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+ 
 bootstrapApplication(App, {
   providers: [provideHttpClient()]
 }).catch(err => console.error(err));
